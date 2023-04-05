bab742e15a557591efdaa2392134f02ee0c2c87d
Lucas Silva <nascimentolucas786@gmail.com>
2023-04-05 12:55:16 -0300

    Implementando matrícula

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -17,11 +17,6 @@ public class AlunoController {
     @Autowired
     private AlunoServiceImpl service;
 
-    @GetMapping
-    public List<Aluno> getAll() {
-        return service.getAll();
-    }
-
     @PostMapping
     public Aluno create(@Valid @RequestBody AlunoForm form) {
         return service.create(form);
@@ -30,6 +25,11 @@ public class AlunoController {
     @GetMapping("/avaliacoes/{id}")
     public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id) {
         return service.getAllAvaliacaoFisicaId(id);
+    }
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNascimento) {
+        return service.findByDataDeNascimento(dataDeNascimento);
     }
 
 }
